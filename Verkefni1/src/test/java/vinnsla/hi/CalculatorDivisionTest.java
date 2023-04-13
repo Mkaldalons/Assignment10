db2a80bed7f088eee51115c4e8b96d1b95fbee02
Maria Ros <mrk9@hi.is>
2023-04-13 14:33:50 +0000

    CalculatorDivisionTest bætt við

--- Verkefni1/src/test/java/vinnsla/hi/CalculatorDivisionTest.java
+++ Verkefni1/src/test/java/vinnsla/hi/CalculatorDivisionTest.java
@@ -1,6 +1,8 @@
 package vinnsla.hi;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class CalculatorDivisionTest{
     private Calculator calculator;
@@ -8,6 +10,13 @@ public class CalculatorDivisionTest{
     @Before
     public void createCalculator(){
         this.calculator = new Calculator();
+    }
+    @Test
+    public void testDivision(){
+        calculator.setOperatorIndex(2);
+        calculator.setNum1(6);
+        calculator.setNum2(3);
+        Assert.assertEquals(2, calculator.calculate());
     }
 
 }
