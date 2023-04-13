4d577e40a164a98a22a5b06341c7c094386ebc43
Maria Ros <mrk9@hi.is>
2023-04-13 14:32:12 +0000

    CalculatorSubtractionTest 1 prófunartilvik Skrifað

---
+++ Verkefni1/src/test/java/vinnsla/hi/CalculatorDivisionTest.java
@@ -0,0 +1,13 @@
+package vinnsla.hi;
+
+import org.junit.Before;
+
+public class CalculatorDivisionTest{
+    private Calculator calculator;
+
+    @Before
+    public void createCalculator(){
+        this.calculator = new Calculator();
+    }
+
+}
--- Verkefni1/src/test/java/vinnsla/hi/CalculatorSubtractionTest.java
+++ Verkefni1/src/test/java/vinnsla/hi/CalculatorSubtractionTest.java
@@ -1,7 +1,23 @@
 package vinnsla.hi;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-public class CalculatorSubtractionTest extends TestCase {
+public class CalculatorSubtractionTest {
+    private Calculator calculator;
 
+    @Before
+    public void createCalculator() {
+        this.calculator = new Calculator();
+    }
+
+    @Test
+    public void testSubtraction() {
+        calculator.setOperatorIndex(1);
+        calculator.setNum1(3);
+        calculator.setNum2(2);
+        Assert.assertEquals(1, calculator.calculate());
+    }
 }
