fa6284066d34c72ffd5484ecfd402129bd480f2d
Maria Ros <mrk9@hi.is>
2023-04-13 14:38:33 +0000

    AllTestsCalculator bætt við

---
+++ Verkefni1/src/test/java/vinnsla/hi/AllTestsCalculator.java
@@ -0,0 +1,10 @@
+package vinnsla.hi;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CalculatorAdditionTest.class, CalculatorDivisionTest.class, CalculatorMultiplicationTest.class, CalculatorSubtractionTest.class})
+public class AllTestsCalculator {
+}
