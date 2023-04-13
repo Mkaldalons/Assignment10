6ca8a57fac06fdba7c4c4a3b656eb245e4974992
Maria Ros <mrk9@hi.is>
2023-04-13 14:36:06 +0000

    CalculatorMultiplicationTest bætt við

---
+++ Verkefni1/src/test/java/vinnsla/hi/CalculatorMultiplicationTest.java
@@ -0,0 +1,20 @@
+package vinnsla.hi;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorMultiplicationTest {
+    private Calculator calculator;
+    @Before
+    public void createCalculator(){
+        this.calculator = new Calculator();
+    }
+    @Test
+    public void testMultiplication(){
+        calculator.setOperatorIndex(3);
+        calculator.setNum1(5);
+        calculator.setNum2(3);
+        Assert.assertEquals(15, calculator.calculate());
+    }
+}
