b4b186d19bf947af24ba334597d11b517450c94d
Maria Ros <mrk9@hi.is>
2023-04-18 10:36:59 +0000

    gitignore

---
+++ Verkefni1/src/test/java/vidmot/hi/OperatorHandlerTest.java
@@ -0,0 +1,10 @@
+package vidmot.hi;
+
+import org.junit.Before;
+import org.junit.Test;
+import vinnsla.hi.Calculator;
+
+public class OperatorHandlerTest  {
+    private Calculator calculator;
+
+}
